091d56865d4f48ee288838a2144e8147f8ad7b58
Victor Owino <vo4685336@gmail.com>
2025-07-17 22:56:56 +0300

    correct the typo to match settings object

--- car_game/urls.py
+++ car_game/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [path("admin/", admin.site.urls), path("", include("main.urls"))]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIR)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
